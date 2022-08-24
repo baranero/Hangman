491b706641615a211341b8b25a037148c5da9bab
baranero <94863094+baranero@users.noreply.github.com>
2022-08-24 23:39:17 +0200

    Display message

--- index.js
+++ index.js
@@ -2,52 +2,70 @@ const startButton = document.getElementsByClassName("start-button")[0];
 let alphabetRow = document.getElementsByClassName("alphabet")[0];
 let wordRow = document.getElementById("word");
 const letterButtons = document.getElementsByClassName("single-letter");
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const messageEl = document.getElementById("message");
+const chancesEl = document.getElementById("chances");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let words = ["HUMAN", "CITY", "CAR", "DOG", "AHAHAHA"];
 let word = words[Math.floor(Math.random() * words.length)];
+let wordSpace = [];
+let chances = 10;
 
 startButton.addEventListener("click", function startGame() {
+    addAlphabetToDesktop();
+    addPlaceForLetter();
+    chancesEl.textContent = `You have ${chances} chances`;
+}, { once: true });
+
+function addAlphabetToDesktop() {
     for (let i = 0; i < alphabet.length; i++) {
         alphabetRow.innerHTML += `<input type="button" class="single-letter" value="${alphabet[i]}">`;
     };
-    wordSpace = [];
+}
+
+function addPlaceForLetter() {
     for (let i = 0; i < word.length; i++) {
         wordSpace.push(" _");
         wordRow.innerText += wordSpace[i];
     }
-
     console.log(word);
-    console.log(wordSpace);
-}, { once: true });
+}
+
+function checkPlaceOfLetterInWord(e) {
+    for (let i = 0; i < 1; i++) {
+        letterInWord = [];
+        startIndex = 0;
+        while ((index = word.indexOf((e.target.value), startIndex)) > -1) {
+            letterInWord.push(index);
+            startIndex = index + e.target.value.length;
+        }
+    } 
+}
+
+function displayCorrectLetter(e) {
+    for (let i = 0; i < letterInWord.length; i++) {
+        wordSpace[letterInWord[i]] = e.target.value;
+    }
+    withoutCommas = wordSpace.join(" ");
+    wordRow.innerText = withoutCommas;
+}
 
 document.body.addEventListener('click', function (e) {
     if (e.target.className == "single-letter") {
         for (let i = 0; i < 1; i++) {
             if (word.includes(e.target.value)) {
-                console.log(e.target.value);
-                for (let i = 0; i < 1; i++) {
-                    //wordRow.textContent += e.target.value
-                    letterInWord = [];
-                    startIndex = 0;
-                    while ((index = word.indexOf((e.target.value), startIndex)) > -1) {
-                        letterInWord.push(index);
-                        startIndex = index + e.target.value.length;
-                    }
-
-                }
-                console.log(letterInWord);
-                for (let i = 0; i < letterInWord.length; i++) {
-                    wordSpace[letterInWord[i]] = e.target.value;
-                }
-                console.log(wordSpace);
-                withoutCommas = wordSpace.join(" ");
-                console.log(withoutCommas);
-                wordRow.innerText = withoutCommas;
-
+                checkPlaceOfLetterInWord(e);
+                displayCorrectLetter(e);
+                messageEl.textContent = "Great! Keep going."
             } else {
                 console.log("Wrong letter");
+                messageEl.textContent = "Wrong letter! Try again."
+                chances -= 1;
+                console.log(chances);
+                chancesEl.textContent = `You have ${chances} chances`;
             }
+            
         }
     }
 });
 
+console.log(chances);
