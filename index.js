21d6bfd82228a43d9457b6f11fa8e9dabc8a10b2
baranero <94863094+baranero@users.noreply.github.com>
2022-09-06 23:14:15 +0200

    win and game over
    new functionalities added - the game ends when you guess the word or use the chances

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const startButton = document.getElementsByClassName("start-button")[0];
+const startButton = document.getElementsByClassName("start-button")[0];   
 let alphabetRow = document.getElementsByClassName("alphabet")[0];
 let wordRow = document.getElementById("word");
 const letterButtons = document.getElementsByClassName("single-letter");
 const messageEl = document.getElementById("message");
 const chancesEl = document.getElementById("chances");
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let words = ["HUMAN", "CITY", "CAR", "DOG", "AHAHAHA"];
-let word = words[Math.floor(Math.random() * words.length)];
-let wordSpace = [];
-let chances = 10;
+let words = ["HUMAN", "AHAHAHA"];
+let word = words[Math.floor(Math.random() * words.length)];     //draw a word from array
+let wordSpace = [];     //underscore in the number of letters in the word
+let chances = 10;       
+let amountOfCorrectLetters = 0;  //
 
 startButton.addEventListener("click", function startGame() {
     addAlphabetToDesktop();
     addPlaceForLetter();
     chancesEl.textContent = `You have ${chances} chances`;
-}, { once: true });
+}, { once: true });     
 
 function addAlphabetToDesktop() {
     for (let i = 0; i < alphabet.length; i++) {
@@ -44,28 +45,47 @@ function checkPlaceOfLetterInWord(e) {
 function displayCorrectLetter(e) {
     for (let i = 0; i < letterInWord.length; i++) {
         wordSpace[letterInWord[i]] = e.target.value;
+        amountOfCorrectLetters += 1;
     }
     withoutCommas = wordSpace.join(" ");
     wordRow.innerText = withoutCommas;
+    
 }
 
-document.body.addEventListener('click', function (e) {
+function checkWin() {
+    if (amountOfCorrectLetters === word.length) {
+        return (messageEl.textContent = "You win!")
+    } 
+}
+
+function hangman(e) {
     if (e.target.className == "single-letter") {
-        for (let i = 0; i < 1; i++) {
             if (word.includes(e.target.value)) {
                 checkPlaceOfLetterInWord(e);
                 displayCorrectLetter(e);
-                messageEl.textContent = "Great! Keep going."
-            } else {
+                messageEl.textContent = "Great! Keep going.";
+                if (amountOfCorrectLetters === word.length) {
+                    document.body.removeEventListener('click', hangman)
+                    return (messageEl.textContent = "You win!");
+                }
+                console.log(amountOfCorrectLetters);
+                console.log(word.length);
+            } 
+            else if (chances <= 1) {
+                chances = 0
+                chancesEl.textContent = `You have ${chances} chances`;
+                document.body.removeEventListener('click', hangman)
+                return (messageEl.textContent = "Game over!")
+            } 
+            else if (word.includes(e.target.value) == false) {
                 console.log("Wrong letter");
                 messageEl.textContent = "Wrong letter! Try again."
                 chances -= 1;
                 console.log(chances);
                 chancesEl.textContent = `You have ${chances} chances`;
-            }
-            
+                
+            } 
         }
     }
-});
 
-console.log(chances);
+document.body.addEventListener('click', hangman)
